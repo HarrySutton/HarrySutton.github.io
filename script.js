9e5b0f581f0744892f88614ed68408596b307a2e
HarrySutton <56020961+HarrySutton@users.noreply.github.com>
2022-06-17 10:34:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ HTMLElement.prototype.$ = function(...args){
     this.appendChild($(...args))
 }
 
+const $id = document.getElementById;
+const $$class = document.getElementsByClassName;
+const $class = c => $$class(c)[0];
+const $$tag = document.getElementsByTagName;
+const $tag = t => $$tag(t)[0];
+
 const pageName = window.location.pathname.split("/").pop().slice(0, -5);
 
 const navdata = [
@@ -33,7 +39,7 @@ const navdata = [
     },
 ];
 
-const navbar = document.getElementsByTagName("nav")[0];
+const navbar = $tag("nav")[0];
 navbar.innerHTML = "";
 
 for (let {name, link} of navdata){
