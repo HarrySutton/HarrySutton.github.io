92b5347597cb13190f3427e7fa3bca01c12bcce9
HarrySutton <56020961+HarrySutton@users.noreply.github.com>
2022-08-15 15:01:47 +0100

    Create jss.ts

---
+++ jss.ts
@@ -0,0 +1,57 @@
+namespace yes{
+    
+}
+
+class JSS{
+
+    root: HTMLElement
+
+    [index: number]: HTMLElement;    
+
+    constructor(root: HTMLElement){
+        this.root = root;
+        return this
+    }
+
+    tag(tag: string){
+
+    }
+
+    // .
+    class(name: string){
+
+    }
+    // #
+    id(id: string){
+
+    }
+
+    // *
+    star(){
+
+    }
+
+    // " "
+    sub(){
+
+    }
+    
+    // ,
+    and(){
+
+    }
+
+    // >
+    child(){
+
+    }
+
+    // +
+    after(){
+
+    }
+}
+
+HTMLCollection
+
+const jss = new JSS(document.documentElement);
