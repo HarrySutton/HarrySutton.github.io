c2ea06419aa380a11d5cf66023755927425834af
HarrySutton <56020961+HarrySutton@users.noreply.github.com>
2024-03-26 14:00:28 +0000

    yes

--- script.js
+++ script.js
@@ -12,33 +12,36 @@ function imgLoad(img, src){
 // Makes the nav buttons stay down after being clicked
 document.querySelectorAll("a.card.press").forEach(a => a.addEventListener("mousedown", e => e.target.className += " active"))
 
-const modal = document.createElement("div");
-modal.className = "modal";
-modal.style.visibility = "hidden";
-modal.style.opacity = 0;
+const dialog = document.createElement("dialog");
 
 const modalimg = document.createElement("img");
 const modalcap = document.createElement("p");
 
-modal.append(modalimg)
-modal.append(modalcap)
-document.body.append(modal)
+dialog.append(modalimg)
+dialog.append(modalcap)
+document.body.append(dialog)
 
-modal.addEventListener("click", e => {
-    modal.style.opacity = 0;
-    modal.style.visibility = "hidden";
+dialog.addEventListener("click", e => {
+    dialog.close()
 })
 
 document.querySelectorAll("img:not(.modal *)").forEach(img => {
-    img.addEventListener("click", e => {
+    img.addEventListener("click", async e => {
 
         modalcap.innerHTML = e.target.alt
 
-        imgLoad(modalimg, e.target.src)
-            .then(() => {
-                modal.style.visibility = "visible";
-                modal.style.opacity = 1
-            })
+        modalimg.src = e.target.src
+
+        // Waits for image to load before displaying the modal
+        await new Promise(res => modalimg.onload = res)
+        
+        dialog.showModal()
+
+        // imgLoad(modalimg, e.target.src)
+        //     .then(() => {
+        //         modal.style.visibility = "visible";
+        //         modal.style.opacity = 1
+        //     })
         
     })
 })
